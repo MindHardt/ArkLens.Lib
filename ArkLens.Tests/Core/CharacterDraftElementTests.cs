1ee32a08ee46c7527e3b511090dfc2ab53f53d8c
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-01-18 02:28:00 +0500

    Added tests

---
+++ ArkLens.Tests/Core/CharacterDraftElementTests.cs
@@ -0,0 +1,45 @@
+﻿using ArkLens.Core;
+using ArkLens.Models.Races;
+using ArkLens.Models.Races.Common;
+using System.Text.Json;
+
+namespace ArkLens.Tests.Core;
+
+public class CharacterDraftElementTests
+{
+	[Fact]
+	public void AdjustTests()
+	{
+		CharacterDraftElement<Race> draft = new();
+
+		Assert.Null(draft.Name);
+		Assert.Null(draft.Value);
+
+		Race race = Dwarf.Value;
+		draft.Name = race.Name;
+
+		Assert.Equal(draft.Value, race);
+
+		CharacterDraftElement<Race> other = new();
+		other.Value = race;
+
+		Assert.Equal(other.Name, race.Name);
+		Assert.Equal(draft, other);
+	}
+
+	[Fact]
+	public void JsonTests()
+	{
+		CharacterDraftElement<Race> draft = new()
+		{
+			Value = Dwarf.Value,
+		};
+
+		string json = JsonSerializer.Serialize(draft);
+		var deserialized = JsonSerializer.Deserialize<CharacterDraftElement<Race>>(json);
+
+		Assert.Equal(draft, deserialized);
+		// CharacterDraftElements are serialized by names so they must not contain emoji
+		Assert.DoesNotContain(draft.Value.Emoji, json);
+	}
+}
---
+++ ArkLens.Tests/Models/Races/RaceTests.cs
@@ -0,0 +1,30 @@
+﻿using ArkLens.Models.Races;
+using ArkLens.Models.Races.Common;
+using System.Linq;
+
+namespace ArkLens.Tests.Models.Races;
+
+public class RaceTests
+{
+	[Fact]
+	public void EnumerationTests()
+	{
+		Assert.NotNull(Race.PossibleValues);
+		Assert.Contains(Human.Value, Race.PossibleValues);
+		Assert.Contains(Elf.Value, Race.PossibleValues);
+		Assert.Contains(Dwarf.Value, Race.PossibleValues);
+		Assert.Contains(Kitsune.Value, Race.PossibleValues);
+		Assert.Contains(Minas.Value, Race.PossibleValues);
+		Assert.Contains(Serpent.Value, Race.PossibleValues);
+	}
+
+	[Fact]
+	public void InfluencesTest()
+	{
+		Assert.Null(Human.Value.StatInfluence);
+		foreach (Race race in Race.PossibleValues.Except(new[] { Human.Value }))
+		{
+			Assert.NotNull(race.StatInfluence);
+		}
+	}
+}
