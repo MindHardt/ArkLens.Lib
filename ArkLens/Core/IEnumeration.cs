ef50ff372757c2b7d907c5af759fe93f4dbecc94
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-01-19 00:40:00 +0500

    Fixed typo

--- ArkLens/Core/IEnumeration.cs
+++ ArkLens/Core/IEnumeration.cs
@@ -9,7 +9,7 @@ public interface IEnumeration<TSelf>
 	where TSelf : IEnumeration<TSelf>
 {
 	/// <summary>
-	/// Represets all possible values of <typeparamref name="TSelf"/>.
+	/// Represents all possible values of <typeparamref name="TSelf"/>.
 	/// </summary>
 	static abstract IReadOnlyList<TSelf> PossibleValues { get; }
 }
