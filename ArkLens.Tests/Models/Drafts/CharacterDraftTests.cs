9a5e993ac5f799056b16c0cfe6c2131b23fcede2
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-02-08 23:33:38 +0500

    Added character element tests

---
+++ ArkLens.Tests/Models/Drafts/CharacterDraftTests.cs
@@ -0,0 +1,20 @@
+﻿using ArkLens.Models.Drafts;
+using System.ComponentModel;
+
+namespace ArkLens.Tests.Models.Drafts;
+
+public class CharacterDraftTests
+{
+    [Fact]
+    public void TestEvents()
+    {
+        CharacterDraft draft = new();
+
+        draft.PropertyChanged += (s, e) =>
+        {
+            Assert.True(e.PropertyName is nameof(draft.Name));
+        };
+
+        draft.Name = "Test";
+    }
+}
