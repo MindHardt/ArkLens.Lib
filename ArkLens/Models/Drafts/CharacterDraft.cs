dc4d399fd159b6b8dba70ff21075773a43c13193
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-02-08 23:32:47 +0500

    Moved CharacterDraft and added blank MonsterDraft

---
+++ ArkLens/Models/Drafts/CharacterDraft.cs
@@ -0,0 +1,41 @@
+﻿using ArkLens.Core;
+using ArkLens.Models.Alignments;
+using ArkLens.Models.Races;
+using ArkLens.Models.Stats;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ArkLens.Models.Drafts;
+
+public class CharacterDraft : INotifyPropertyChanged
+{
+    private string? name;
+
+    public string? Name
+    {
+        get => name;
+        set
+        {
+            name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public CharacterDraftElement<Alignment> Alignment { get; } = new();
+
+    public CharacterDraftElement<Race> Race { get; } = new();
+
+    public StatSet Stats { get; init; } = new();
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+    public CharacterDraft()
+    {
+        Alignment.PropertyChanged += (_, _) => OnPropertyChanged(nameof(Alignment));
+        Race.PropertyChanged += (_, _) => OnPropertyChanged(nameof(Race));
+    }
+}
---
+++ ArkLens/Models/Drafts/MonsterDraft.cs
@@ -0,0 +1,42 @@
+﻿using ArkLens.Core;
+using ArkLens.Models.Alignments;
+
+namespace ArkLens.Models.Drafts;
+
+public class MonsterDraft
+{
+    /// <summary>
+    /// The name of this <see cref="MonsterDraft"/>
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// The <see cref="Alignments.Alignment"/> of this <see cref="MonsterDraft"/>
+    /// </summary>
+    public CharacterDraftElement<Alignment> Alignment { get; } = new();
+
+    public int Hp { get; set; } = 10;
+    public string? HealthDice { get; set; }
+
+    public int Str { get; set; } = 0;
+    public int Dex { get; set; } = 0;
+    public int Con { get; set; } = 0;
+    public int Int { get; set; } = 0;
+    public int Wis { get; set; } = 0;
+    public int Cha { get; set; } = 0;
+
+    public int Fort { get; set; } = 0;
+    public int Reac { get; set; } = 0;
+    public int Will { get; set; } = 0;
+    public int Conc { get; set; } = 0;
+    public int Perc { get; set; } = 0;
+
+    public int Base { get; set; } = 16;
+    public int Touc { get; set; } = 16;
+    public int Unaw { get; set; } = 16;
+
+    public string? Lore { get; set; }
+    public string? Attacks { get; set; }
+    public string? Additions { get; set; }
+    public string? AvatarBase64 { get; set; }
+}
