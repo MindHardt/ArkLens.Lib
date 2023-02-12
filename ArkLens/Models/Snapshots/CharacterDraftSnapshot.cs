e64dc1fedffb3261e1bf64bc8353ec7ece0b0cd7
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-02-12 18:53:14 +0500

    Removed ID from model

--- ArkLens/Models/Snapshots/CharacterDraftSnapshot.cs
+++ ArkLens/Models/Snapshots/CharacterDraftSnapshot.cs
@@ -4,7 +4,6 @@ namespace ArkLens.Models.Snapshots;
 
 public record CharacterDraftSnapshot : ISnapshot<CharacterDraftSnapshot, CharacterDraft>
 {
-	public ulong? Id { get; init; }
 	public required string? Name { get; init; }
 	//public required string? Class { get; init; }
 	public required string? Race { get; init; }
