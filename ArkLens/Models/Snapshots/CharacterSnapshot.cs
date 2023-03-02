4f81bdc48871482965e092d55685ff448540b3aa
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-03-02 18:10:43 +0500

    Added static fabric methods to IBuilder and ISnapshot.

--- ArkLens/Models/Snapshots/CharacterSnapshot.cs
+++ ArkLens/Models/Snapshots/CharacterSnapshot.cs
@@ -3,13 +3,22 @@ using ArkLens.Models.Builders;
 
 namespace ArkLens.Models.Snapshots;
 
-public record CharacterSnapshot : ISnapshot<CharacterBuilder>
+public record CharacterSnapshot : ISnapshot<CharacterBuilder, CharacterSnapshot>
 {
 	public string? Name { get; set; }
 	//public required string? Class { get; init; }
 	public string? Race { get; set; }
 	public string? Alignment { get; set; }
 	public string? Sex { get; set; }
+
+	public static CharacterSnapshot FromBuilder(CharacterBuilder builder)
+		=> new()
+		{
+			Name = builder.Name,
+			Race = builder.Race.Name,
+			Alignment = builder.Alignment.Name,
+			Sex = builder.Sex.Name,
+		};
 
 	public CharacterBuilder CreateBuilder()
 		=> new()
