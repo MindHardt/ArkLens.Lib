c23c7126218f8f3b8b661bbfbdbaf4d1daf735ec
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-03-09 17:39:11 +0500

    Added barbarian stats priority

--- ArkLens/Models/Classes/Common/Barbarian.cs
+++ ArkLens/Models/Classes/Common/Barbarian.cs
@@ -1,6 +1,7 @@
 ﻿using ArkLens.Core;
 using ArkLens.Models.Alignments;
 using ArkLens.Models.Stats;
+using ArkLens.Models.Stats.Primary;
 using System.Collections.Immutable;
 
 namespace ArkLens.Models.Classes.Common;
@@ -19,6 +20,7 @@ public class Barbarian : Class, ISingleton<Barbarian>
     public override int SkillPoints => 3;
 
     public override StatsPrority StatsPrority { get; }
+        = StatsPrority.Create<Strength, Constitution, Dexterity, Wisdom, Charisma, Intelligence>();
 
 	public override IReadOnlySet<Alignment>? AllowedAlignments 
         => Alignment.All.Where(a => a.Horizontal != HorizontalAlignment.Lawful).ToImmutableHashSet();
