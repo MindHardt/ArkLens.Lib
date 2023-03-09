6a8c859b0dd6143d02ac0c5c34808da643247f4e
MindHardt <71509337+MindHardt@users.noreply.github.com>
2023-03-09 17:44:08 +0500

    Added SingletonHelper xmldocs

--- ArkLens/Extensions/SingletonHelper.cs
+++ ArkLens/Extensions/SingletonHelper.cs
@@ -3,8 +3,15 @@ using System.Reflection;
 
 namespace ArkLens.Extensions;
 
+/// <summary>
+/// A class that contains all <see cref="ISingleton{TSelf}.Static"/> values.
+/// </summary>
 public static class SingletonHelper
 {
+	/// <summary>
+	/// Contains all <see cref="ISingleton{TSelf}"/> values
+	/// casted to <see cref="object"/>?.
+	/// </summary>
 	public static IList<object?> AllSingletons { get; }
 		= GetSingletonValues();
 
@@ -12,7 +19,9 @@ public static class SingletonHelper
 	{
 		var prop = GetProperty().Name;
 		var singletonTypes = GetSingletonTypes();
-		return singletonTypes.Select(st => st.GetProperty(prop)!.GetValue(null)).ToArray();
+		return singletonTypes
+			.Select(st => st.GetProperty(prop)!.GetValue(null))
+			.ToArray();
 	}
 	private static PropertyInfo GetProperty() => typeof(ISingleton<>).GetProperties().Single();
 	private static IEnumerable<Type> GetSingletonTypes() => Assembly.GetExecutingAssembly()
